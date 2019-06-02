36e6d36bdd767becad3bfd008d3c7a762f19dec9
Devashish Puri <devashishpuri@gmail.com>
2019-06-02 20:17:29 +0530

    Initial Implementation

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # broadcast_events
-Enables you to broadcast events in your app
+
+Enables you to broadcast events in your app.
+
+## Usage
+
+```dart
+/// Subscription Example
+BroadcastEvents().subscribe<String>('CUSTOM_EVENT', (message) {
+    print(message);
+});
+
+BroadcastEvents()
+    .publish<String>('CUSTOM_EVENT', arguments: 'Hello Subscribers');
+
+/// Unsubscription Example
+final _handler = (int code) => print('The Code is: $code');
+
+BroadcastEvents().subscribe<int>('CUSTOM_EVENT_2', _handler);
+
+BroadcastEvents().unsubscribe<int>('CUSTOM_EVENT_2', handler: _handler);
+
+BroadcastEvents().publish<int>('CUSTOM_EVENT_2', arguments: 2);
+```
+
---
+++ lib/broadcast_events.dart
@@ -0,0 +1,86 @@
+library broadcast_events;
+
+typedef EventHandler<T> = void Function(T args);
+
+/// The Custom Events Service that can publish data
+/// from one module/component to another.
+class BroadcastEvents {
+  static final BroadcastEvents _singleton = new BroadcastEvents._internal();
+
+  Map<String, dynamic> _callbackMaps = Map<String, dynamic>();
+
+  factory BroadcastEvents() {
+    return _singleton;
+  }
+
+  BroadcastEvents._internal();
+
+  /// Subscribe to an event topic. Events that
+  /// get posted to that topic will trigger
+  /// the provided handler
+  /// * [topic] The topic to subscribe to
+  /// * [handler] The event handler
+  subscribe<T>(String topic, EventHandler<T> handler) {
+    List<EventHandler<T>> handlers = _callbackMaps[topic];
+    if (handlers == null) {
+      handlers = [];
+      _callbackMaps[topic] = handlers;
+    }
+    handlers.add(handler);
+  }
+
+  /// Unsubscribe from the given topic. Your handler
+  /// will no longer recieve events published to this
+  /// topic
+  /// * [topic] The topic to unsubscribe from
+  /// * [handler] The event handler
+  unsubscribe<T>(String topic, {EventHandler<T> handler}) {
+    if (handler == null) {
+      _callbackMaps.remove(topic);
+      return;
+    }
+
+    final handlers = _callbackMaps[topic];
+    if (handlers == null) {
+      return;
+    }
+
+    // We now need to remove specific handler
+    final handlerIndex = handlers.indexOf(handler);
+
+    if (handlerIndex >= 0) {
+      handlers.removeAt(handlerIndex);
+      if (handlers.length == 0) {
+        _callbackMaps.remove(topic);
+      }
+    }
+  }
+
+  /// Publish an Event to given topic.
+  /// * [topic] The topic to publish to
+  /// * [arguments] The arguments to pass to
+  /// the handlers. It's better if custom
+  /// Structure is used.
+  publish<T>(String topic, {T arguments}) {
+    List<EventHandler<T>> handlers;
+    try {
+      handlers = _callbackMaps[topic];
+    } catch (_) {
+      throw FormatException(
+          'Type mismatch between Publisher Arguments and what Subscriber Expects');
+    }
+
+    if (handlers == null || handlers.length == 0) {
+      return;
+    }
+
+    handlers.forEach((EventHandler<T> handler) {
+      try {
+        handler(arguments);
+      } catch (_) {
+        print('Error Occured while executing handler for topic $topic');
+        print(_);
+      }
+    });
+  }
+}
